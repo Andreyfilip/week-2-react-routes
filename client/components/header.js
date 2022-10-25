4243ff4181d3c062373d0470107083b12b391bc9
Andreyfilip <98354768+Andreyfilip@users.noreply.github.com>
2022-10-25 00:31:02 -0400

    Update header.js

--- client/components/header.js
+++ client/components/header.js
@@ -21,13 +21,13 @@ const Header = () => {
           className="font-semibold text-xl tracking-tight"
           onClick={() => toggle(!toggled)}
         >
-         Click on me a little baby and I will show you someone very cute
+         Привіт натисни на мене and I will show you someone very cute! 🥰 
         </button>
       </div>
       {toggled && (
         <div id="menu" className="block">
           <a
-            href="https://www.instagram.com/k_lujza/"
+            href="https://www.instagram.com/dasha_sidenko/?igshid=YmMyMTA2M2Y%3D"
             className="flex items-center px-3 py-2 border rounded text-gray-700 border-gray-700 hover:text-white hover:border-white"
           >
             <svg
